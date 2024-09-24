4b3eae370988df768a6e831d376b293b2f0431de
erlint1212 <e.t.nordtvedt@gmail.com>
2024-09-24 13:39:59 +0200

    Fixed Ex1 and asked TA about some things, theory is still fked

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           rev = "b87ca50c8c4d5a5876333b598aed4eb84de925a3";
           sha256 = "12mhmmibizbxgmsns80c8h97rr7rclv9hz98zpgsl26hw3s4l0vm";
         };
-   propagatedBuildInputs = with pkgs.rPackages; [caret mlbench dplyr ggplot2 knitr devtools shiny reticulate Metrics];
+   propagatedBuildInputs = with pkgs.rPackages; [caret mlbench dplyr ggplot2 knitr devtools shiny reticulate Metrics ppcor];
       };
     in {
       devShells.default = pkgs.mkShell {
